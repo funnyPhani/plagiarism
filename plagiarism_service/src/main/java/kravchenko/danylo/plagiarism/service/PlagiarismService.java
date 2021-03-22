4cb51ba6a9d134cd7b74f55dee419e45b64ed461
UndeadBigUnicorn <kravchel16@gmail.com>
2021-03-22 11:58:10 +0200

    add frontend templates

--- plagiarism_service/src/main/java/kravchenko/danylo/plagiarism/service/PlagiarismService.java
+++ plagiarism_service/src/main/java/kravchenko/danylo/plagiarism/service/PlagiarismService.java
@@ -15,9 +15,6 @@ import org.springframework.stereotype.Service;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.OptionalDouble;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @Slf4j
 @Service
